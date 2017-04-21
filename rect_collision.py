d6640406dddb23b7035097a050db34b344a9a1a7
seul lee <blacksangi14@naver.com>
2017-04-21 15:43:47 +0900

    no message

---
+++ rect_collision.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+
+
+import os
+import random
+import pygame
+
+
+class Player(object):
+    
+    def __init__(self):
+        self.rect = pygame.Rect(32, 32, 16, 16)
+
+    def move(self, dx, dy):
+        
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+    
+    def move_single_axis(self, dx, dy):
+        
+        self.rect.x += dx
+        self.rect.y += dy
+
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:
+                    self.rect.right = wall.rect.left
+                if dx < 0: 
+                    self.rect.left = wall.rect.right
+                if dy > 0:
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:
+                    self.rect.top = wall.rect.bottom
+
+class Wall(object):
+    
+    def __init__(self, pos):
+        walls.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.init()
+
+pygame.display.set_caption("Get to the red square!")
+screen = pygame.display.set_mode((320, 240))
+
+clock = pygame.time.Clock()
+walls = [] 
+fails = []
+player = Player() 
+
+level = [
+"WWWWWWWWWWWWWWWWWWWW",
+"W F                W",
+"WF  F     WWWWWW   W",
+"W   WWWW       W   W",
+"W F W        WWWW  W",
+"W WWW FWWWW        W",
+"W   W  F  W W      W",
+"W   W     W   WWW WW",
+"W   WWW WWW   W W  W",
+"W     W   W   W W  W",
+"WWW   W F WWWWW W  W",
+"W W      WW        W",
+"W WF  WWWWF FWWWF  W",
+"W     W    E   W   W",
+"WWWWWWWWWWWWWWWWWWWW",
+]
+
+x = y = 0
+for row in level:
+    for col in row:
+        if col == "W":
+            Wall((x, y))
+        if col == "E":
+            end_rect = pygame.Rect(x, y, 16, 16)
+        if col == "F":
+            fail_rect = pygame.Rect(x, y, 16, 16)
+            fails.append(fail_rect)
+        x += 16
+    y += 16
+    x = 0
+
+running = True
+while running:
+    
+    clock.tick(60)
+    
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
+    
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player.move(-2, 0)
+    if key[pygame.K_RIGHT]:
+        player.move(2, 0)
+    if key[pygame.K_UP]:
+        player.move(0, -2)
+    if key[pygame.K_DOWN]:
+        player.move(0, 2)
+    
+    if player.rect.colliderect(end_rect):
+        raise SystemExit("You win!")
+    
+    for fail in fails:
+        if player.rect.colliderect(fail):
+            raise SystemExit("Fail!")
+
+
+    screen.fill((0, 0, 0))
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+    pygame.draw.rect(screen, (255, 0, 0), end_rect)
+    pygame.draw.rect(screen, (255, 200, 0), player.rect)
+    for fail in fails:
+        pygame.draw.rect(screen, (0, 255, 255), fail)
+    
+    pygame.display.flip()
